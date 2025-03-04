5a1e0e57a6cbeaefc3ed2a51493ad5666cb75a3e
Rustam Aliev <donko.donko583@gmail.com>
2025-03-04 14:44:16 +0300

    chore: удалил ненужные папки

--- src/pages/PayPage/ui/styles.ts
+++ src/pages/PayPage/ui/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Field, Form } from 'formik';
+import { Form } from 'formik';
 
 export const Container = styled(Form)`
     max-width: 1280px;
@@ -14,47 +14,4 @@ export const Container = styled(Form)`
     }
 `;
 
-export const SecondSection = styled.div`
-    font-size: 18px;
-    color: #444;
-    line-height: 1.8;
-    background: rgba(255, 255, 255, 0.7);
-    padding: 15px;
-    border-radius: 10px;
-    width: 100%;
-    box-shadow: ${(props) => props.theme.shadow.default};
-    height: fit-content;
 
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-`;
-
-export const Input = styled(Field)`
-    padding: 1rem;
-    font-size: ${({ theme }) => theme.fontSizes.default};
-    border: ${(props) => props.theme.colors.border};
-    border-radius: 10px;
-
-    &:focus {
-        outline: none;
-    }
-`;
-
-export const TotalPrice = styled.p`
-    font-size: 18px;
-    font-weight: 500;
-    color: ${(props) => props.theme.colors.text};
-    width: fit-content;
-    margin-left: 1rem;
-
-    strong {
-        font-weight: 700;
-        color: ${(props) => props.theme.colors.primary};
-    }
-`;
-
-export const Line = styled.hr`
-    width: 90%;
-    margin: 0 auto;
-`;
